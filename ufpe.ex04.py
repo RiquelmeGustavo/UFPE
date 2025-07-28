82817e226d1f41444fccee5551a847d421082f5b
RiquelmeGustavo <mcriquelme10@gmail.com>
2025-07-28 15:41:28 -0300

    Update ufpe.ex04.py

--- ufpe.ex04.py
+++ ufpe.ex04.py
@@ -1,4 +1,8 @@
-# Construir um programa que faça a conversão de temperatura de Celcius para Fahrenheit. (F = 9 * C / 5 + 32)
+"""
+Arquivo: ufpe.ex04.py
+Autor: Riquelme Gustavo
+Descrição: Construir um programa que faça a conversão de temperatura de Celcius para Fahrenheit. (F = 9 * C / 5 + 32)
+"""
 
 
 print('Converter Celcius em Fahrenheit')
