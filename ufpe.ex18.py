669097f690edbe6e948834b69ea43ffdd87decdf
RiquelmeGustavo <mcriquelme10@gmail.com>
2025-07-29 14:51:32 -0300

    Update ufpe.ex18.py

--- ufpe.ex18.py
+++ ufpe.ex18.py
@@ -1,6 +1,10 @@
-''' A sequência de Fibonacci é a sequência (1, 1, 2, 3, 5, 8, 13,...)
+"""
+Arquivo: ufpe.ex18.py
+Autor: Riquelme Gustavo
+Descrição: A sequência de Fibonacci é a sequência (1, 1, 2, 3, 5, 8, 13,...)
 definida por: an = {1 se n=1 ou n=2}{an-1 + an-2 se n>2}. Escreva um programa que determine o n-ésimo termo desta
-sequência, n dado. '''
+sequência, n dado.
+""" 
 
 
 print('Sequência de Fibonacci')
