1d64c3339083c92c9f4409b643631b3813685c99
RiquelmeGustavo <mcriquelme10@gmail.com>
2025-07-29 15:10:56 -0300

    Update ufpe.ex20.py

--- ufpe.ex20.py
+++ ufpe.ex20.py
@@ -1,3 +1,14 @@
+"""
+Arquivo: ufpe.ex20.py
+Autor: Riquelme Gustavo
+Descrição: Escreva um programa que transforme o computador numa urna eletrônica para eleição para presidente do país fictício, às quais concorrem 
+os candidatos 83-Alibabá e 93-Alcapone. Cada voto deve ser dado pelo número do candidato, permitindo-se ainda o voto 00 para voto em branco. 
+Qualquer voto diferente dos já citados é considerado nulo; em qualquer situação, o eleitor deve ser consultado quanto à confirmação do seu voto. 
+No final da eleição, o programa deve emitir um relatório contendo a votação de cada candidato, a quantidade de votos em branco, a quantidade de 
+votos nulos e o candidato eleito.
+"""
+
+
 print('Eleições para Presidente do Brasil')
 print('83 - Alibabá \n93 - Alcapone')
 
