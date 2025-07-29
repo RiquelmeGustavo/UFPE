16813694100c66b8c33774cda35d5d031419ba0b
RiquelmeGustavo <mcriquelme10@gmail.com>
2025-07-29 15:07:20 -0300

    Update ufpe.ex19.py

--- ufpe.ex19.py
+++ ufpe.ex19.py
@@ -1,3 +1,14 @@
+"""
+Arquivo: ufpe.ex19.py
+Autor: Riquelme Gustavo
+Descrição: A série harmônica S(n), com n sendo um inteiro, é uma serie divergente. Isto significa que dado qualquer real k 
+existe um n0 tal que S(n0) > k. Escreva um programa que dado um real k determine o menor inteiro no tal que S(no) > k. 
+Por exemplo se k=2, o programa deve fornecer n0 = 4, pois S(4) = 2,083... e S(3) = 1,8333...
+
+S(n) = 1/i = 1 + 1/2 + 1/3 +...+ 1/n
+"""
+
+
 k = int(input('Digite o valor de k: '))
 S = n0 = 1
 
