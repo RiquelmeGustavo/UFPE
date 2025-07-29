8e29b2251d87c869f0472e2848323df66672d464
RiquelmeGustavo <mcriquelme10@gmail.com>
2025-07-29 14:47:39 -0300

    Update ufpe.ex17.py

--- ufpe.ex17.py
+++ ufpe.ex17.py
@@ -1,7 +1,11 @@
-''' Um número inteiro é dito perfeito se o dobro dele é igual à soma de todos os seus divisores.
+"""
+Arquivo: ufpe.ex17.py
+Autor: Riquelme Gustavo
+Descrição: Um número inteiro é dito perfeito se o dobro dele é igual à soma de todos os seus divisores.
 Por exemplo, como os divisores de 6 são 1, 2, 3 e 6 e 1 + 2 + 3 + 6 = 12, 6 é perfeito. A matemática ainda não sabe
 se a quantidade de números perfeitos é ou não finita. Escreva um programa que liste todos os números perfeitos menores
-que um inteiro n dado. '''
+que um inteiro n dado.
+""" 
 
 
 num = int(input('Digite um número para ver os perfeitos anteriores: '))
